f2ffb9fe6803c3d006bf0f32a96022964d8963cb
KeiIchi-74 <72638679+KeiIchi-74@users.noreply.github.com>
2020-10-26 09:26:50 +0900

    setting association
    usersテーブルと、itemsテーブルのアソシエーションを各モデルに記述した

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :user
   belongs_to_active_hash :category, :sales_status, :shipping_fee, :prefecture, :scheduled_delivery
   has_one_attached :image
 end
