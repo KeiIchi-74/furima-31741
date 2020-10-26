9da0bb49fec47554aa032e254893f2cb900853ec
KeiIchi-74 <72638679+KeiIchi-74@users.noreply.github.com>
2020-10-26 09:44:48 +0900

    setting validation
    itemsテーブルのバリデーションを設定した。

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,16 @@ class Item < ApplicationRecord
   belongs_to :user
   belongs_to_active_hash :category, :sales_status, :shipping_fee, :prefecture, :scheduled_delivery
   has_one_attached :image
+
+  validates :image, presence: true
+  validates :name, presence: true, length: { maximum: 40 }
+  validates :info, presence: true, length: { maximum: 1000 }
+  validates :category, :sales_status, :shipping_fee, :prefecture, :scheduled_delivery, presence: true
+  validates :category_id, numericality: { other_than: 1}
+  validates :sales_status_id, numericality: { other_than: 1}
+  validates :shipping_fee_id, numericality: { other_than: 1}
+  validates :prefecture_id, numericality: { other_than: 1}
+  validates :scheduled_delivery_id, numericality: { other_than: 1}
+  validates :price, presence: true, format: { with: /\A\d+\z/ }, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, }
+  validates :user_id, presence: true
 end
