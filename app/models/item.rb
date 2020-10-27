9e0768e0541f7b30bd05de7a0524ed7761521169
KeiIchi-74 <72638679+KeiIchi-74@users.noreply.github.com>
2020-10-27 09:47:43 +0900

    Implementation of unit test code
    itemモデルの単体テストコードを作成した。

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,7 +11,6 @@ class Item < ApplicationRecord
   validates :image, presence: true
   validates :name, presence: true, length: { maximum: 40 }
   validates :info, presence: true, length: { maximum: 1000 }
-  validates :category, :sales_status, :shipping_fee, :prefecture, :scheduled_delivery, presence: true
   with_options numericality: { other_than: 1, message: "Select" } do
     validates :category_id
     validates :sales_status_id
@@ -19,11 +18,15 @@ class Item < ApplicationRecord
     validates :prefecture_id
     validates :scheduled_delivery_id
   end
-  validates :price, presence: true, format: { with: /\A\d+\z/, message: "Half-width number" }, 
+  validates :price, presence: true, 
+  format: { 
+    with: /\A\d+\z/, 
+    message: "Half-width number" 
+  }, 
   numericality: { 
     greater_than_or_equal_to: 300, 
+    message: "Out of setting range", 
     less_than_or_equal_to: 9999999, 
     message: "Out of setting range" 
   }
-  validates :user_id, presence: true
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    name                  {Faker::Lorem.characters(number: 40)}
+    info                  {Faker::Lorem.characters(number: 1000)}
+    price                 {1111}
+    association           :category
+    association           :sales_status
+    association           :shipping_fee
+    association           :prefecture
+    association           :scheduled_delivery
+    association           :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open("public/images/test_image.png"), filename: "test_image.png")
+    end
   end
 end
