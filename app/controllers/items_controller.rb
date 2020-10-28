6465b8bdac9143b3448c56261dd908252e14fabe
KeiIchi-74 <72638679+KeiIchi-74@users.noreply.github.com>
2020-10-28 21:30:21 +0900

    Implementation of product information update function
    商品の情報を編集して、更新が行えるようにした。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:show, :edit]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.order("created_at DESC")
@@ -22,6 +22,14 @@ class ItemsController < ApplicationController
   end
 
   def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(item)
+    else
+      render :edit
+    end 
   end
 
   private
