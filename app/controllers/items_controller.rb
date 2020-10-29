377b8ac3c84e319c6831c26ec65db66606618d83
KeiIchi-74 <72638679+KeiIchi-74@users.noreply.github.com>
2020-10-29 11:16:02 +0900

    modification items_controller.rb
    出品したユーザーでないと削除できないようにした

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :move_to_top, only: [:edit]
+  before_action :move_to_top, only: [:edit, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
