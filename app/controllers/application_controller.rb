bcffc88f1e38e42196cf4ecd380b7509bc05f96e
KeiIchi-74 <72638679+KeiIchi-74@users.noreply.github.com>
2020-10-31 12:23:17 +0900

    Introduction Basic authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!, only: :new
+  before_action :basic_auth
 
   private
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [
       :nickname, 
@@ -12,5 +14,11 @@ class ApplicationController < ActionController::Base
       :first_name_kana, 
       :birth_date
       ])
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
   end
 end
