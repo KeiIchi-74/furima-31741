2c8974e22bdfdf544dc2f6cdfbfe0b8d4aa9a35b
KeiIchi-74 <72638679+KeiIchi-74@users.noreply.github.com>
2020-10-31 11:03:50 +0900

    modification order_form.rb
    presennce: trueの記述を繰り返し行っていたので、with_optionsを用いてまとめた。

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -3,18 +3,21 @@ class OrderForm
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id, :token
 
-  validates :token, presence: true
-  validates :postal_code, presence: true, format: { 
-    with: /\A[0-9]{3}-[0-9]{4}\z/, 
-    message: "Input correctly" 
-  }
-  validates :prefecture_id, presence: true, numericality: { other_than: 1, message: "Select" }
-  validates :city, presence: true
-  validates :house_number, presence: true
-  validates :phone_number, presence: true, format: { 
-    with: /\A[0-9]+\z/, 
-    message: "Input only number" 
-  }
+  with_options presence: true do
+    validates :token
+    validates :postal_code, format: { 
+      with: /\A[0-9]{3}-[0-9]{4}\z/, 
+      message: "Input correctly" 
+    }
+    validates :prefecture_id, numericality: { other_than: 1, message: "Select" }
+    validates :city
+    validates :house_number
+    validates :phone_number, format: { 
+      with: /\A[0-9]+\z/, 
+      message: "Input only number" 
+    }
+  end
+  
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
