067f4225dcdbae867afaadbf8c6ac40c5025540e
KeiIchi-74 <72638679+KeiIchi-74@users.noreply.github.com>
2020-11-13 00:46:23 +0900

    複数画像保存のためのコントローラーと、各デザインの修正
    複数画像が保存でき、編集も行えるように、コントローラでのストロングパラメーターを設定し、トップページと、商品詳細ページの画像の表記に関するコードを修正した。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,9 @@ class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update, :destroy, :save]
   before_action :move_to_top, only: [:edit, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :edit_ids, only: :update
+  before_action :delete_ids, only: :update
+  before_action :set_item, only: :update
 
   def index
     @items = Item.order("created_at DESC")
@@ -12,7 +15,7 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.new(item_params)
+    @item = Item.new(item_images_params)
     if @item.save
       redirect_to root_path
     else
@@ -29,7 +32,7 @@ class ItemsController < ApplicationController
 
   def update
     if @item.update(item_params)
-      redirect_to item_path(@item)
+      redirect_to item_path(@item.id)
     else
       render :edit
     end 
@@ -42,7 +45,7 @@ class ItemsController < ApplicationController
 
   private
 
-  def item_params
+  def item_images_params
     params.require(:item).permit(
       :name, 
       :info, 
@@ -56,17 +59,71 @@ class ItemsController < ApplicationController
     ).merge(user_id: current_user.id)
   end
 
+  def item_params
+    params.require(:item).permit(
+      :name, 
+      :info, 
+      :category_id, 
+      :sales_status_id, 
+      :shipping_fee_id, 
+      :prefecture_id, 
+      :scheduled_delivery_id, 
+      :price
+    ).merge(user_id: current_user.id)
+  end
+
+  def edit_ids_params
+    params.require(:item).permit(edit_ids: []).merge(user_id: current_user.id)
+  end
+
+  def delete_ids_params
+    params.require(:item).permit(delete_ids: []).merge(user_id: current_user.id)
+  end
+
+  def images_params
+    params.require(:item).permit(images: []).merge(user_id: current_user.id)
+  end
+
   def set_item
     @item = Item.find(params[:id])
   end
 
   def move_to_top
-    @item = Item.find(params[:id])
+    set_item
     if user_signed_in?
       if @item.user_id != current_user.id 
         redirect_to root_path
       elsif @item.order.present?
         redirect_to root_path
+      end
+    end
+  end
+
+  def edit_ids
+    set_item
+    if edit_ids_params.present?
+      edit_ids_params.each do |edit_id|
+        image = @item.images.find(edit_id)
+        image.purge
+      end
+    end
+  end
+
+  def delete_ids
+    set_item
+    if delete_ids_params.present?
+      delete_ids_params.each do |delete_id|
+        image = @item.images.find(delete_id)
+        image.purge
+      end
+    end
+  end
+
+  def set_image
+    set_item
+    if images_params.present?
+      images_params.each do |image|
+        @item.images.attach(image)
       end
     end
   end
