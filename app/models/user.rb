d5f8761ec7006242c9f968a5a7785fa11ac575cf
KeiIchi-74 <72638679+KeiIchi-74@users.noreply.github.com>
2020-11-20 04:15:27 +0900

    sns認証の実装
    sns認証を、新規登録画面、ログイン画面の２つで実装した。

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,8 +17,10 @@ class User < ApplicationRecord
   has_many :messages
   has_many :message_items, through: :messages, source: :item
 
+  has_many :sns_credentials
+
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
          
   validates :nickname, presence: true, uniqueness: { case_sensitive: true }
   validates :birth_date, presence: true
@@ -38,6 +40,19 @@ class User < ApplicationRecord
 
   def followed_by?(user)
     passive_relationships.find_by(following_id: user.id).present?
+  end
+
+  def self.from_omniauth(auth)
+    sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
+    user = User.where(email: auth.info.email).first_or_initialize(
+      nickname: auth.info.name,
+      email: auth.info.email
+    )
+    if user.persisted?
+      sns.user = user
+      sns.save
+    end
+    { user: user, sns: sns }
   end
 
 end
